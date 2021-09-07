1411be7d9d3371e1cfa225a1822600903d377ce7
Matt Carrier <icecreammatt@gmail.com>
2021-09-07 12:00:52 -0700

    feat: add dev scripts

---
+++ keyboards/atreus/keymaps/icecreammatt/gen-layout.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+qmk c2json -km icecreammatt -kb atreus/astar /Users/matt/qmk_firmware/keyboards/atreus/keymaps/icecreammatt/keymap.c --no-cpp -o layout.json
+
+source replace-keymap-with-numbers.sh
+
+
