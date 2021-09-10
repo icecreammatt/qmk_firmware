a7af1f37acf9416fe504592bee3edb077d3bd880
Matt Carrier <icecreammatt@gmail.com>
2021-09-10 01:26:59 -0700

    style: indenting

--- keyboards/atreus/keymaps/icecreammatt/keymap.c
+++ keyboards/atreus/keymaps/icecreammatt/keymap.c
@@ -100,17 +100,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   // QWERTY Plain
   [QWERTY_PLN] = LAYOUT(
-    KC_Q,        KC_W,      KC_E,      KC_R,      KC_T,                         KC_Y,    KC_U,    KC_I,      KC_O,        KC_P,
-    KC_A,        KC_S,      KC_D,      KC_F,      KC_G,                         KC_H,    KC_J,    KC_K,      KC_L,        KC_SCLN,
-    LSFT_T(KC_Z),KC_X,      KC_C,      KC_V,      KC_B,                         KC_N,    KC_M,    KC_COMM,   KC_DOT,      RSFT_T(KC_SLSH),
+    KC_Q,        KC_W,      KC_E,      KC_R,      KC_T,                         KC_Y,    KC_U,     KC_I,      KC_O,        KC_P,
+    KC_A,        KC_S,      KC_D,      KC_F,      KC_G,                         KC_H,    KC_J,     KC_K,      KC_L,        KC_SCLN,
+    LSFT_T(KC_Z),KC_X,      KC_C,      KC_V,      KC_B,                         KC_N,    KC_M,     KC_COMM,   KC_DOT,      RSFT_T(KC_SLSH),
     KC_TRNS,     TO(QWERTY),KC_TRNS,   KC_LGUI,   KC_TRNS,  KC_TRNS, KC_TRNS,   KC_TRNS, MO(POK3R),KC_TRNS,  KC_TRNS,     KC_TRNS
   ),
 
   // COLEMAK Plain
   [COLEMAK_PLN] = LAYOUT(
-    KC_Q,        KC_W,      KC_F,      KC_P,      KC_B,                         KC_J,    KC_L,    KC_U,      KC_Y,        KC_SCLN,
-    KC_A,        KC_R,      KC_S,      KC_T,      KC_G,                         KC_M,    KC_N,    KC_E,      KC_I,        KC_O,
-    LSFT_T(KC_Z),KC_X,      KC_C,      KC_D,      KC_V,                         KC_K,    KC_H,    KC_COMM,   KC_DOT,      RSFT_T(KC_SLSH),
+    KC_Q,        KC_W,      KC_F,      KC_P,      KC_B,                         KC_J,    KC_L,     KC_U,      KC_Y,        KC_SCLN,
+    KC_A,        KC_R,      KC_S,      KC_T,      KC_G,                         KC_M,    KC_N,     KC_E,      KC_I,        KC_O,
+    LSFT_T(KC_Z),KC_X,      KC_C,      KC_D,      KC_V,                         KC_K,    KC_H,     KC_COMM,   KC_DOT,      RSFT_T(KC_SLSH),
     KC_TRNS,     TO(COLEMAK),KC_TRNS,  KC_LGUI,   KC_TRNS,  KC_TRNS, KC_TRNS,   KC_TRNS, MO(POK3R),KC_TRNS,   KC_TRNS,     KC_TRNS
   ),
 
