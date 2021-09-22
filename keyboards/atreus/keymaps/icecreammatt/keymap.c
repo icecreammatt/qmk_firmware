e2a8191c48af09bed19659b57aafa8d4d243dd92
Matt Carrier <icecreammatt@gmail.com>
2021-09-21 22:48:03 -0700

    fix: gaming layer should have all LGUI keys so when windows layer is has LGUI swapped with control they are all ctrl

--- keyboards/atreus/keymaps/icecreammatt/keymap.c
+++ keyboards/atreus/keymaps/icecreammatt/keymap.c
@@ -56,12 +56,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_GRV,  KC_Q,          KC_X,             KC_E,             KC_R,                       KC_T,        KC_Y,                KC_U,    KC_I,      KC_O,
     KC_LSFT, KC_A,          KC_W,             KC_D,             KC_F,                       KC_G,        KC_H,                KC_J,    KC_K,      KC_L,
     KC_LGUI, KC_Z,          KC_S,             KC_C,             KC_V,                       KC_B,        KC_N,                KC_M,    KC_UP,     KC_P,
-    KC_TAB,  MO(NUMERIC),   MO(NUM_SPECIAL),  MO(NUM_SPECIAL),  KC_SPC,  KC_LCTL, KC_TRNS,  KC_BSPC,     KC_ENT,              KC_LEFT, KC_DOWN,   KC_RIGHT
+    KC_TAB,  MO(NUMERIC),   MO(NUM_SPECIAL),  MO(NUM_SPECIAL),  KC_SPC,  KC_LGUI, KC_TRNS,  KC_BSPC,     KC_ENT,              KC_LEFT, KC_DOWN,   KC_RIGHT
   ),
 
   // Numeric
   [NUMERIC] = LAYOUT(
-    KC_DEL,     KC_F1,    KC_F2,    KC_F3,    KC_F10,                                     KC_INS,     KC_PGUP,    KC_HOME,            KC_PGDN,      KC_DEL,
+    KC_DEL,     KC_F1,    KC_F2,    KC_F3,    KC_F10,                                     KC_INS,     KC_PGDN,    KC_HOME,            KC_PGUP,      KC_DEL,
     KC_SLCK,    KC_F4,    KC_F5,    KC_F6,    KC_F11,                                     KC_LEFT,    KC_DOWN,    KC_UP,              KC_RIGHT,     KC_MUTE,
     KC_LSFT,    KC_F7,    KC_F8,    KC_F9,    KC_F12,                                     KC_MPLY,    KC_WH_U,    KC_END,             KC_WH_D,      LSFT_T(KC_EQL),
     KC_CAPS,    KC_TRNS,  KC_PSCR,  KC_PAUS,  KC_TRNS,  LCTL_T(KC_SPC),  LALT_T(KC_ESC),  CG_NORM,    KC_VOLD,    LALT_T(KC_ESC),     KC_VOLU,      CG_SWAP
