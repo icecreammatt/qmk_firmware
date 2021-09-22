40e125069688077af46b90f2598b210618706b90
Matt Carrier <icecreammatt@gmail.com>
2021-09-21 21:31:19 -0700

    feat: swap tab with ` on stdg (gaming layer) swap w, s, x around for improved comfort for fps replace TO(QWERTY) key with ENT since can be accessed by MO(NUM_SPECIAL) key and clicking the TO(QWERTY) key

--- keyboards/atreus/keymaps/icecreammatt/keymap.c
+++ keyboards/atreus/keymaps/icecreammatt/keymap.c
@@ -53,10 +53,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   // Gaming II
   [STDG] = LAYOUT(
-    KC_TAB,  KC_Q,     KC_W,             KC_E,             KC_R,                       KC_T,        KC_Y,                KC_U,    KC_I,      KC_O,
-    KC_LCTL, KC_A,     KC_S,             KC_D,             KC_F,                       KC_G,        KC_H,                KC_J,    KC_K,      KC_L,
-    KC_LSFT, KC_Z,     KC_X,             KC_C,             KC_V,                       KC_B,        KC_N,                KC_M,    KC_UP,     KC_P,
-    KC_GRV,  KC_NO,    MO(NUM_SPECIAL),  MO(NUMERIC),      KC_SPC,  KC_TRNS, KC_TRNS,  KC_BSPC,     TO(QWERTY),          KC_LEFT, KC_DOWN,   KC_RIGHT
+    KC_GRV,  KC_Q,          KC_X,             KC_E,             KC_R,                       KC_T,        KC_Y,                KC_U,    KC_I,      KC_O,
+    KC_LSFT, KC_A,          KC_W,             KC_D,             KC_F,                       KC_G,        KC_H,                KC_J,    KC_K,      KC_L,
+    KC_LGUI, KC_Z,          KC_S,             KC_C,             KC_V,                       KC_B,        KC_N,                KC_M,    KC_UP,     KC_P,
+    KC_TAB,  MO(NUMERIC),   MO(NUM_SPECIAL),  MO(NUM_SPECIAL),  KC_SPC,  KC_LCTL, KC_TRNS,  KC_BSPC,     KC_ENT,              KC_LEFT, KC_DOWN,   KC_RIGHT
   ),
 
   // Numeric
