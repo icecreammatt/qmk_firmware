4bd38daffb4b1590b0f7c3409a31e0d6fc4538ab
Matt Carrier <icecreammatt@gmail.com>
2022-03-23 20:35:53 -0700

    fix: wip

--- keyboards/bastardkb/charybdis/3x5/keymaps/icecreammatt/keymap.c
+++ keyboards/bastardkb/charybdis/3x5/keymaps/icecreammatt/keymap.c
@@ -16,6 +16,107 @@
  */
 #include QMK_KEYBOARD_H
 
+typedef enum {
+    TD_NONE,
+    TD_UNKNOWN,
+    TD_SINGLE_TAP,
+    TD_SINGLE_HOLD,
+    TD_DOUBLE_TAP,
+    TD_DOUBLE_HOLD,
+    TD_DOUBLE_SINGLE_TAP, // Send two single taps
+    TD_TRIPLE_TAP,
+    TD_TRIPLE_HOLD
+} td_state_t;
+
+typedef struct {
+    bool       is_press_action;
+    td_state_t state;
+} td_tap_t;
+
+// Tap dance enums
+// X_CTL,
+enum {
+    LGUI_ALT = 0,
+    LCTL_NAV,
+    EXLM_UNDS,
+    LBRC_AMPR,
+    RBRC_PIPE,
+    PAST_HASH,
+    AT_DOT,
+    CT_CLN,
+    SLSH_BSLS,
+    Q_ENT,
+    PGDN_PGUP,
+    // W_TAB,
+    ESC_MOUSE,
+    HASH_DOT,
+    // CIRC_HASH,
+
+    SOME_OTHER_DANCE
+};
+
+td_state_t cur_dance(qk_tap_dance_state_t *state);
+
+// Left-hand home row mods
+#define HOME_A LGUI_T(KC_A)
+#define HOME_S LALT_T(KC_S)
+#define HOME_D LCTL_T(KC_D)
+#define HOME_F LSFT_T(KC_F)
+
+// Right-hand home row mods
+#define HOME_J RSFT_T(KC_J)
+#define HOME_K RCTL_T(KC_K)
+#define HOME_L LALT_T(KC_L)
+#define HOME_QUOT RGUI_T(KC_QUOT)
+
+#define QWERTY 0
+#define COLEMAK 1
+#define SYMBOL 2
+#define FNAV 3
+#define MOUSE 4
+#define QWERTY_2 5
+
+// clang-format off
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+  [QWERTY] = LAYOUT_charybdis_3x5(
+  // ╭─────────────────────────────────────────────╮ ╭─────────────────────────────────────────────╮
+          KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,       KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
+  // ├─────────────────────────────────────────────┤ ├─────────────────────────────────────────────┤
+          KC_A,    KC_S,    KC_D,    KC_F,    KC_G,       KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,
+  // ├─────────────────────────────────────────────┤ ├─────────────────────────────────────────────┤
+          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,       KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,
+  // ╰─────────────────────────────────────────────┤ ├─────────────────────────────────────────────╯
+                         CTL_BSP, SFT_SPC,   LOWER,      RAISE, GUI_ENT
+  //                   ╰───────────────────────────╯ ╰──────────────────╯
+  ),
+
+  [COLEMAK] = LAYOUT_charybdis_3x5(
+  // ╭─────────────────────────────────────────────╮ ╭─────────────────────────────────────────────╮
+       RGB_TOG, KC_MNXT, KC_MPLY, KC_MPRV, XXXXXXX,    KC_LBRC,    KC_7,    KC_8,    KC_9, KC_RBRC,
+  // ├─────────────────────────────────────────────┤ ├─────────────────────────────────────────────┤
+       KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, XXXXXXX,    KC_PPLS,    KC_4,    KC_5,    KC_6, KC_PMNS,
+  // ├─────────────────────────────────────────────┤ ├─────────────────────────────────────────────┤
+       XXXXXXX, XXXXXXX, XXXXXXX, EEP_RST,   RESET,    KC_PAST,    KC_1,    KC_2,    KC_3, KC_PSLS,
+  // ╰─────────────────────────────────────────────┤ ├─────────────────────────────────────────────╯
+                         XXXXXXX, XXXXXXX, _______,    XXXXXXX, _______
+  //                   ╰───────────────────────────╯ ╰──────────────────╯
+  ),
+
+  [QWERTY_2] = LAYOUT_charybdis_3x5(
+  // ╭─────────────────────────────────────────────╮ ╭─────────────────────────────────────────────╮
+       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX, KC_VOLU, KC_MUTE, KC_VOLD, XXXXXXX,
+  // ├─────────────────────────────────────────────┤ ├─────────────────────────────────────────────┤
+       KC_LEFT,   KC_UP, KC_DOWN, KC_RGHT, XXXXXXX,    XXXXXXX, KC_RSFT, KC_RCTL, KC_RALT, KC_RGUI,
+  // ├─────────────────────────────────────────────┤ ├─────────────────────────────────────────────┤
+       KC_HOME, KC_PGUP, KC_PGDN,  KC_END, XXXXXXX,      RESET, EEP_RST, XXXXXXX, XXXXXXX, XXXXXXX,
+  // ╰─────────────────────────────────────────────┤ ├─────────────────────────────────────────────╯
+                         _______, _______, XXXXXXX,    _______, XXXXXXX
+  //                   ╰───────────────────────────╯ ╰──────────────────╯
+  ),
+};
+*/
+
+/*
 enum charybdis_keymap_layers {
     LAYER_BASE = 0,
     LAYER_LOWER,
@@ -67,4 +168,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //                   ╰───────────────────────────╯ ╰──────────────────╯
   ),
 };
-// clang-format on
+*/
+
+    // clang-format on
