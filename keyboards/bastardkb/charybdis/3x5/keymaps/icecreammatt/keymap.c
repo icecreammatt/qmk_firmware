a7626e419c3962bff40230a9b6928acab3b187c4
Matt Carrier <icecreammatt@gmail.com>
2022-03-27 17:32:21 -0700

    fix: remove comments and dead code

--- keyboards/bastardkb/charybdis/3x5/keymaps/icecreammatt/keymap.c
+++ keyboards/bastardkb/charybdis/3x5/keymaps/icecreammatt/keymap.c
@@ -49,10 +49,8 @@ typedef struct {
 } td_tap_t;
 
 // Tap dance enums
-// X_CTL,
 enum {
     LGUI_ALT = 0,
-    LCTL_NAV,
     EXLM_UNDS,
     LBRC_AMPR,
     RBRC_PIPE,
@@ -62,7 +60,6 @@ enum {
     SLSH_BSLS,
     Q_ENT,
     PGDN_PGUP,
-    // W_TAB,
     ESC_MOUSE,
     HASH_DOT,
     // CIRC_HASH,
@@ -225,53 +222,11 @@ static td_tap_t guitap_state = {
     .state = TD_NONE
 };
 
-// td_tap_t for LCTL key
-static td_tap_t ctrltap_state = {
-    .is_press_action = true,
-    .state = TD_NONE
-};
-
 // td_tap_t for ESC key
 static td_tap_t esctap_state = {
     .is_press_action = true,
     .state = TD_NONE
 };
-
-// void x_finished(qk_tap_dance_state_t *state, void *user_data) {
-//     xtap_state.state = cur_dance(state);
-//     switch (xtap_state.state) {
-//         case TD_SINGLE_TAP: register_code(KC_X); break;
-//         case TD_SINGLE_HOLD: register_code(KC_LCTRL); break;
-//         case TD_DOUBLE_TAP: register_code(KC_ESC); break;
-//         case TD_DOUBLE_HOLD: register_code(KC_LALT); break;
-//         // Last case is for fast typing. Assuming your key is `f`:
-//         // For example, when typing the word `buffer`, and you want to make sure that you send `ff` and not `Esc`.
-//         // In order to type `ff` when typing fast, the next character will have to be hit within the `TAPPING_TERM`, which by default is 200ms.
-//         case TD_DOUBLE_SINGLE_TAP: tap_code(KC_X); register_code(KC_X);
-//         case TD_UNKNOWN: break;
-//         case TD_NONE: break;
-//         case TD_TRIPLE_TAP: break;
-//         case TD_TRIPLE_HOLD: break;
-//     }
-// }
-
-// void x_reset(qk_tap_dance_state_t *state, void *user_data) {
-//     switch (xtap_state.state) {
-//         case TD_SINGLE_TAP: unregister_code(KC_X); break;
-//         case TD_SINGLE_HOLD: unregister_code(KC_LCTRL); break;
-//         case TD_DOUBLE_TAP: unregister_code(KC_ESC); break;
-//         case TD_DOUBLE_HOLD: unregister_code(KC_LALT);
-//         case TD_DOUBLE_SINGLE_TAP: unregister_code(KC_X);
-//         case TD_UNKNOWN: break;
-//         case TD_NONE: break;
-//         case TD_TRIPLE_TAP: break;
-//         case TD_TRIPLE_HOLD: break;
-//     }
-//     xtap_state.state = TD_NONE;
-// }
-
-
-//////////////////////////////////////////////////////////
 
 void gui_finished(qk_tap_dance_state_t *state, void *user_data) {
     guitap_state.state = cur_dance(state);
@@ -304,45 +259,6 @@ void gui_reset(qk_tap_dance_state_t *state, void *user_data) {
         case TD_TRIPLE_HOLD: break;
     }
     guitap_state.state = TD_NONE;
-}
-
-void ctrl_finished(qk_tap_dance_state_t *state, void *user_data) {
-    ctrltap_state.state = cur_dance(state);
-    switch (ctrltap_state.state) {
-        case TD_SINGLE_TAP: register_code(KC_LCTL); break;
-        case TD_SINGLE_HOLD: register_code(KC_LCTL); break;
-        case TD_DOUBLE_TAP: break;
-        case TD_DOUBLE_HOLD: break; //layer_on(NUMPAD); break;
-        // Last case is for fast typing. Assuming your key is `f`:
-        // For example, when typing the word `buffer`, and you want to make sure that you send `ff` and not `Esc`.
-        // In order to type `ff` when typing fast, the next character will have to be hit within the `TAPPING_TERM`, which by default is 200ms.
-        case TD_DOUBLE_SINGLE_TAP: break;
-        case TD_UNKNOWN: break;
-        case TD_NONE: break;
-        case TD_TRIPLE_TAP: break;
-        case TD_TRIPLE_HOLD: break;
-    }
-}
-
-void ctrl_reset(qk_tap_dance_state_t *state, void *user_data) {
-    switch (ctrltap_state.state) {
-        case TD_SINGLE_TAP: unregister_code(KC_LCTL); break;
-        case TD_SINGLE_HOLD: unregister_code(KC_LCTL); break;
-        case TD_DOUBLE_TAP: break;
-        case TD_DOUBLE_HOLD: break;//{
-        //     if(ctrltap_state.state == TD_DOUBLE_HOLD) {
-        //         layer_off(NUMPAD);
-        //     }
-        //     ctrltap_state.state = TD_NONE;
-        //     break;
-        // };
-        case TD_DOUBLE_SINGLE_TAP: break;
-        case TD_UNKNOWN: break;
-        case TD_NONE: break;
-        case TD_TRIPLE_TAP: break;
-        case TD_TRIPLE_HOLD: break;
-    }
-    ctrltap_state.state = TD_NONE;
 }
 
 //// mouse lock
@@ -415,7 +331,6 @@ void dance_cln_reset(qk_tap_dance_state_t *state, void *user_data) {
 }
 
 qk_tap_dance_action_t tap_dance_actions[] = {
-    // [X_CTL] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, x_finished, x_reset),
     [CT_CLN] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, dance_cln_finished, dance_cln_reset),
     [EXLM_UNDS] = ACTION_TAP_DANCE_DOUBLE(KC_EXLM, KC_UNDS),
     [LBRC_AMPR] = ACTION_TAP_DANCE_DOUBLE(KC_LBRC, KC_AMPR),
@@ -427,15 +342,10 @@ qk_tap_dance_action_t tap_dance_actions[] = {
     // [DLR_UNDS] = ACTION_TAP_DANCE_DOUBLE(KC_DLR, KC_UNDS),
     [Q_ENT] =    ACTION_TAP_DANCE_DOUBLE(KC_Q, KC_ENT),
     [PGDN_PGUP] =    ACTION_TAP_DANCE_DOUBLE(KC_PGDN, KC_PGUP),
-    // [W_TAB] =    ACTION_TAP_DANCE_DOUBLE(KC_W, KC_TAB),
     [LGUI_ALT] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, gui_finished, gui_reset),
     [LGUI_ALT] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, gui_finished, gui_reset),
-    [LCTL_NAV] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, ctrl_finished, ctrl_reset),
     [ESC_MOUSE] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, esc_finished, esc_reset)
 };
-
-
-
 
 
 /*
@@ -452,44 +362,5 @@ enum charybdis_keymap_layers {
 #define SFT_SPC SFT_T(KC_SPC)
 #define GUI_ENT GUI_T(KC_ENT)
 
-// clang-format off
-const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  [LAYER_BASE] = LAYOUT_charybdis_3x5(
-  // ╭─────────────────────────────────────────────╮ ╭─────────────────────────────────────────────╮
-          KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,       KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
-  // ├─────────────────────────────────────────────┤ ├─────────────────────────────────────────────┤
-          KC_A,    KC_S,    KC_D,    KC_F,    KC_G,       KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,
-  // ├─────────────────────────────────────────────┤ ├─────────────────────────────────────────────┤
-          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,       KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,
-  // ╰─────────────────────────────────────────────┤ ├─────────────────────────────────────────────╯
-                         CTL_BSP, SFT_SPC,   LOWER,      RAISE, GUI_ENT
-  //                   ╰───────────────────────────╯ ╰──────────────────╯
-  ),
-
-  [LAYER_LOWER] = LAYOUT_charybdis_3x5(
-  // ╭─────────────────────────────────────────────╮ ╭─────────────────────────────────────────────╮
-       RGB_TOG, KC_MNXT, KC_MPLY, KC_MPRV, XXXXXXX,    KC_LBRC,    KC_7,    KC_8,    KC_9, KC_RBRC,
-  // ├─────────────────────────────────────────────┤ ├─────────────────────────────────────────────┤
-       KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, XXXXXXX,    KC_PPLS,    KC_4,    KC_5,    KC_6, KC_PMNS,
-  // ├─────────────────────────────────────────────┤ ├─────────────────────────────────────────────┤
-       XXXXXXX, XXXXXXX, XXXXXXX, EEP_RST,   RESET,    KC_PAST,    KC_1,    KC_2,    KC_3, KC_PSLS,
-  // ╰─────────────────────────────────────────────┤ ├─────────────────────────────────────────────╯
-                         XXXXXXX, XXXXXXX, _______,    XXXXXXX, _______
-  //                   ╰───────────────────────────╯ ╰──────────────────╯
-  ),
-
-  [LAYER_RAISE] = LAYOUT_charybdis_3x5(
-  // ╭─────────────────────────────────────────────╮ ╭─────────────────────────────────────────────╮
-       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX, KC_VOLU, KC_MUTE, KC_VOLD, XXXXXXX,
-  // ├─────────────────────────────────────────────┤ ├─────────────────────────────────────────────┤
-       KC_LEFT,   KC_UP, KC_DOWN, KC_RGHT, XXXXXXX,    XXXXXXX, KC_RSFT, KC_RCTL, KC_RALT, KC_RGUI,
-  // ├─────────────────────────────────────────────┤ ├─────────────────────────────────────────────┤
-       KC_HOME, KC_PGUP, KC_PGDN,  KC_END, XXXXXXX,      RESET, EEP_RST, XXXXXXX, XXXXXXX, XXXXXXX,
-  // ╰─────────────────────────────────────────────┤ ├─────────────────────────────────────────────╯
-                         _______, _______, XXXXXXX,    _______, XXXXXXX
-  //                   ╰───────────────────────────╯ ╰──────────────────╯
-  ),
-};
 */
 
-    // clang-format on
