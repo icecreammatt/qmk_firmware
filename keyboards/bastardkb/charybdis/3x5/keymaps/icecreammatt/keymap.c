dfe572ef030c4c1fbd402547f0f9c21b0eea40f7
Matt Carrier <icecreammatt@gmail.com>
2022-04-04 18:53:54 -0700

    fix: turn off auto pointer mode

--- keyboards/bastardkb/charybdis/3x5/keymaps/icecreammatt/keymap.c
+++ keyboards/bastardkb/charybdis/3x5/keymaps/icecreammatt/keymap.c
@@ -27,7 +27,7 @@
 // #endif  // CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_ENABLE
 
 #ifdef CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_ENABLE
-static uint16_t auto_pointer_layer_timer = 0;
+// static uint16_t auto_pointer_layer_timer = 0;
 
 // #    ifndef CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_TIMEOUT_MS
 #        define CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_TIMEOUT_MS 1000
@@ -219,35 +219,34 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 // #ifdef POINTING_DEVICE_ENABLE
 // #    ifdef CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_ENABLE
-report_mouse_t pointing_device_task_user(report_mouse_t mouse_report) {
-    if (abs(mouse_report.x) > CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_THRESHOLD ||
-        abs(mouse_report.y) > CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_THRESHOLD) {
-        if (auto_pointer_layer_timer == 0) {
-            layer_on(MOUSE);
-// #        ifdef RGB_MATRIX_ENABLE
-//             rgb_matrix_mode_noeeprom(RGB_MATRIX_NONE);
-//             rgb_matrix_sethsv_noeeprom(HSV_GREEN);
-// #        endif  // RGB_MATRIX_ENABLE
-        }
-        auto_pointer_layer_timer = timer_read();
-    }
-    return mouse_report;
-}
+// report_mouse_t pointing_device_task_user(report_mouse_t mouse_report) {
+//     if (abs(mouse_report.x) > CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_THRESHOLD ||
+//         abs(mouse_report.y) > CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_THRESHOLD) {
+//         if (auto_pointer_layer_timer == 0) {
+//             layer_on(MOUSE);
+// // #        ifdef RGB_MATRIX_ENABLE
+// //             rgb_matrix_mode_noeeprom(RGB_MATRIX_NONE);
+// //             rgb_matrix_sethsv_noeeprom(HSV_GREEN);
+// // #        endif  // RGB_MATRIX_ENABLE
+//         }
+//         auto_pointer_layer_timer = timer_read();
+//     }
+//     return mouse_report;
+// }
 
-void matrix_scan_kb(void) {
-    if (auto_pointer_layer_timer != 0 && TIMER_DIFF_16(timer_read(),
-        auto_pointer_layer_timer) >= CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_TIMEOUT_MS) {
-        auto_pointer_layer_timer = 0;
-        layer_off(MOUSE);
-// #        ifdef RGB_MATRIX_ENABLE
-//         rgb_matrix_mode_noeeprom(RGB_MATRIX_STARTUP_MODE);
-// #        endif  // RGB_MATRIX_ENABLE
-    }
-    matrix_scan_user();
-}
+// void matrix_scan_kb(void) {
+//     if (auto_pointer_layer_timer != 0 && TIMER_DIFF_16(timer_read(),
+//         auto_pointer_layer_timer) >= CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_TIMEOUT_MS) {
+//         auto_pointer_layer_timer = 0;
+//         layer_off(MOUSE);
+// // #        ifdef RGB_MATRIX_ENABLE
+// //         rgb_matrix_mode_noeeprom(RGB_MATRIX_STARTUP_MODE);
+// // #        endif  // RGB_MATRIX_ENABLE
+//     }
+//     matrix_scan_user();
+// }
 // #    endif  // CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_ENABLE
 // #endif      // POINTING_DEVICE_ENABLE
-
 
 
 // clang-format on
