caa5622100c767c251808d631c19fdc301c8ef62
Matt Carrier <icecreammatt@gmail.com>
2022-08-25 00:07:05 -0700

    fix: swap gui and shift on iris

--- keyboards/keebio/iris/keymaps/icecreammatt/keymap.c
+++ keyboards/keebio/iris/keymaps/icecreammatt/keymap.c
@@ -64,16 +64,16 @@ enum {
 td_state_t cur_dance(qk_tap_dance_state_t *state);
 
 // Left-hand home row mods
-#define HOME_A LGUI_T(KC_A)
+#define HOME_A LSFT_T(KC_A)
 #define HOME_S LALT_T(KC_S)
 #define HOME_D LCTL_T(KC_D)
-#define HOME_F LSFT_T(KC_F)
+#define HOME_F LGUI_T(KC_F)
 
 // Right-hand home row mods
-#define HOME_J RSFT_T(KC_J)
+#define HOME_J RGUI_T(KC_J)
 #define HOME_K RCTL_T(KC_K)
 #define HOME_L LALT_T(KC_L)
-#define HOME_QUOT RGUI_T(KC_QUOT)
+#define HOME_QUOT RSFT_T(KC_QUOT)
 
 #define QWERTY_MIN 0
 #define QWERTY 1
