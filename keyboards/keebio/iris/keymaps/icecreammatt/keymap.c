11e7102a1401bf87c16efe345a0c6538995c9057
Matt Carrier <icecreammatt@gmail.com>
2022-08-25 00:09:37 -0700

    fix: swap scroll and volume encoders

--- keyboards/keebio/iris/keymaps/icecreammatt/keymap.c
+++ keyboards/keebio/iris/keymaps/icecreammatt/keymap.c
@@ -289,16 +289,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 bool encoder_update_user(uint8_t index, bool clockwise) {
     if (index == 0) {
         if (clockwise) {
-            tap_code(KC_VOLU);
+            tap_code(KC_WH_D);
         } else {
-            tap_code(KC_VOLD);
+            tap_code(KC_WH_U);
         }
     }
     else if (index == 1) {
         if (clockwise) {
-            tap_code(KC_PGDN);
+            tap_code(KC_VOLU);
         } else {
-            tap_code(KC_PGUP);
+            tap_code(KC_VOLD);
         }
     }
     return false;
