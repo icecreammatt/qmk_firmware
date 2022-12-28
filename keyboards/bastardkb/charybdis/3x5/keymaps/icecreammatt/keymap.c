7ebb9c5856207c26101d1d766ee844d535442cdc
Matt Carrier <icecreammatt@gmail.com>
2022-12-27 16:54:40 -0800

    feat: add double tap for tab and esc

--- keyboards/bastardkb/charybdis/3x5/keymaps/icecreammatt/keymap.c
+++ keyboards/bastardkb/charybdis/3x5/keymaps/icecreammatt/keymap.c
@@ -55,6 +55,8 @@ enum {
     CT_CLN,
     SLSH_BSLS,
     Q_ENT,
+    T_ESC,
+    B_TAB,
     PGDN_PGUP,
     ESC_NAV,
     HASH_DOT,
@@ -142,7 +144,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [COLEMAK] = LAYOUT_charybdis_3x5(
   // ╭────────────────────────────────────────────────────────────────────────╮ ╭────────────────────────────────────────────────────────────────────────╮
-       KC_Q,               KC_W,           KC_F,       KC_P,           KC_B,        KC_J,      KC_L,      KC_U,      KC_Y,              KC_QUOT,
+       TD(Q_ENT),               KC_W,           KC_F,       KC_P,           KC_B,        KC_J,      KC_L,      KC_U,      KC_Y,              KC_QUOT,
   // ├────────────────────────────────────────────────────────────────────────┤ ├────────────────────────────────────────────────────────────────────────┤
        KC_A,               KC_R,           KC_S,       KC_T,           KC_G,        KC_M,      KC_N,      KC_E,      KC_I,              LT(MOUSE, KC_O),
   // ├────────────────────────────────────────────────────────────────────────┤ ├────────────────────────────────────────────────────────────────────────┤
@@ -179,11 +181,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [GAMING] = LAYOUT_charybdis_3x5(
   // ╭────────────────────────────────────────────────────────────────────────╮ ╭────────────────────────────────────────────────────────────────────────╮
-       KC_T,         KC_Q,             KC_W,             KC_E,        KC_R,                 KC_Y,    KC_U,      KC_I,           KC_O,          KC_P,
+       TD(T_ESC),    KC_Q,             KC_W,             KC_E,        KC_R,                 KC_Y,    KC_U,      KC_I,           KC_O,          KC_P,
   // ├────────────────────────────────────────────────────────────────────────┤ ├────────────────────────────────────────────────────────────────────────┤
        LSFT_T(KC_G), KC_A,             KC_S,             KC_D,        KC_F,                 KC_H,    KC_J,      KC_K,           KC_L,          LT(MOUSE, KC_QUOT),
   // ├────────────────────────────────────────────────────────────────────────┤ ├────────────────────────────────────────────────────────────────────────┤
-       LSFT_T(KC_B), KC_Z,             KC_X,             KC_C,        KC_V,                 KC_N,    KC_M,      KC_COMM,        KC_DOT,        RSFT_T(KC_SLSH),
+       TD(B_TAB),    KC_Z,             KC_X,             KC_C,        KC_V,                 KC_N,    KC_M,      KC_COMM,        KC_DOT,        RSFT_T(KC_SLSH),
   // ╰────────────────────────────────────────────────────────────────────────┤ ├────────────────────────────────────────────────────────────────────────╯
                          MO(GAMING_2), KC_SPC,           KC_LCTL,                   LT(NAV, KC_ESC),    LT(SYMBOL, KC_BSPC)
   //                   ╰──────────────────────────────────────────────────────╯ ╰────────────────────────────────────────────╯
@@ -191,11 +193,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [GAMING_2] = LAYOUT_charybdis_3x5(
   // ╭────────────────────────────────────────────────────────────────────────╮ ╭────────────────────────────────────────────────────────────────────────╮
-       KC_ESC,         KC_7,         KC_8,        KC_9,        KC_0,                 KC_Y,    KC_U,      KC_I,           KC_O,          KC_P,
+       KC_ESC,         KC_1,         KC_2,        KC_3,        KC_0,                 KC_Y,    KC_U,      KC_I,           KC_O,          KC_P,
   // ├────────────────────────────────────────────────────────────────────────┤ ├────────────────────────────────────────────────────────────────────────┤
-       KC_TAB,         KC_1,         KC_2,        KC_3,        KC_I,                 KC_H,    KC_J,      KC_K,           KC_L,          LT(MOUSE, KC_QUOT),
+       KC_ENT,         KC_4,         KC_5,        KC_6,        KC_I,                 KC_H,    KC_J,      KC_K,           KC_L,          LT(MOUSE, KC_QUOT),
   // ├────────────────────────────────────────────────────────────────────────┤ ├────────────────────────────────────────────────────────────────────────┤
-       KC_ENT,         KC_4,         KC_5,        KC_6,        KC_M,                 KC_N,    KC_M,      KC_COMM,        KC_DOT,        RSFT_T(KC_SLSH),
+       KC_TAB,         KC_7,         KC_8,        KC_9,        KC_M,                 KC_N,    KC_M,      KC_COMM,        KC_DOT,        RSFT_T(KC_SLSH),
   // ╰────────────────────────────────────────────────────────────────────────┤ ├────────────────────────────────────────────────────────────────────────╯
                          _______,    _______,     _______,                           LT(NAV, KC_ESC),    LT(SYMBOL, KC_BSPC)
   //                   ╰──────────────────────────────────────────────────────╯ ╰────────────────────────────────────────────╯
@@ -484,6 +486,9 @@ qk_tap_dance_action_t tap_dance_actions[] = {
 
     [CIRC_HOME] = ACTION_TAP_DANCE_DOUBLE(KC_CIRC, KC_HOME),
     [DLR_END] = ACTION_TAP_DANCE_DOUBLE(KC_DLR, KC_END),
+
+    [B_TAB] = ACTION_TAP_DANCE_DOUBLE(KC_B, KC_TAB),
+    [T_ESC] = ACTION_TAP_DANCE_DOUBLE(KC_T, KC_ESC),
 
     [Q_ENT] = ACTION_TAP_DANCE_DOUBLE(KC_Q, KC_ENT),
     [PGDN_PGUP] = ACTION_TAP_DANCE_DOUBLE(KC_PGDN, KC_PGUP),
