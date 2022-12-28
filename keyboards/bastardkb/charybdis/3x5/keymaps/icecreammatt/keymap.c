3351bd801607fe2e7c7444a20cefac1279d394cb
Matt Carrier <icecreammatt@gmail.com>
2022-12-27 20:32:53 -0800

    refactor: move homerow mods into separate file

--- keyboards/bastardkb/charybdis/3x5/keymaps/icecreammatt/keymap.c
+++ keyboards/bastardkb/charybdis/3x5/keymaps/icecreammatt/keymap.c
@@ -18,30 +18,7 @@
 #include "g/keymap_combo.h" // might not need this
 #include "defines.h"
 #include "tapdance.h"
-
-// Left-hand home row mods
-#define HOME_A LSFT_T(KC_A)
-#define HOME_S LALT_T(KC_S)
-#define HOME_D LCTL_T(KC_D)
-#define HOME_F LGUI_T(KC_F)
-
-// Right-hand home row mods
-#define HOME_J LGUI_T(KC_J)
-#define HOME_K RCTL_T(KC_K)
-#define HOME_L LALT_T(KC_L)
-#define HOME_QUOT RSFT_T(KC_QUOT)
-
-// Left-hand home row mods colemak
-#define HOME_AC LSFT_T(KC_A)
-#define HOME_R LALT_T(KC_R)
-#define HOME_SC LCTL_T(KC_S)
-#define HOME_T LGUI_T(KC_T)
-
-// Right-hand home row mods colemak
-#define HOME_N LGUI_T(KC_N)
-#define HOME_E RCTL_T(KC_E)
-#define HOME_I LALT_T(KC_I)
-#define HOME_O RSFT_T(KC_O)
+#include "homerow.h"
 
 // Layer Defines
 #define QWERTY 0
